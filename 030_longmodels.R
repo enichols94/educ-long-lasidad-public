1a7524c5420dca0c9a1ecd741303e325252ccaad
Emma Nichols <emmanich@dts.usc.edu>
2025-01-22 17:26:22 -0800

    add stratifications, and change method for quantile regression

--- 030_longmodels.R
+++ 030_longmodels.R
@@ -13,6 +13,8 @@ pacman::p_load(data.table, openxlsx, haven, readr, dplyr, magrittr, stringr,
 date <- gsub("-", "_", Sys.Date())
 set.seed(6541)
 
+## add gender to stratifications
+
 # SET OBJECTS -------------------------------------------------------------
 
 dropbox_dir <- "C:/Users/emmanich/P2AGING Dropbox/Emma Nichols/"
@@ -86,32 +88,22 @@ get_model <- function(model_data){
 
 m5 <- get_model(dt[age_group == "60-69"]); m6 <- get_model(dt[age_group == "70-79"]); m7 <- get_model(dt[age_group == "80+"])
 m8 <- get_model(dt[rural == 0]); m9 <- get_model(dt[rural == 1])
-m10 <- get_model(dt[above_w1medcog == 0]); m11 <- get_model(dt[above_w1medcog == 1])
-m12 <- get_model(dt[above_w2medfu == 0]); m13 <- get_model(dt[above_w2medfu == 1])
+m10 <- get_model(dt[female == 0]); m11 <- get_model(dt[female == 1])
+m12 <- get_model(dt[above_w1medcog == 0]); m13 <- get_model(dt[above_w1medcog == 1])
+m14 <- get_model(dt[above_w2medfu == 0]); m15 <- get_model(dt[above_w2medfu == 1])
+m16 <- get_model(dt[cdr == "Normal"]); m17 <- get_model(dt[cdr == "MCI/Questionnable"]); m18 <- get_model(dt[cdr == "Dementia"])
 
 ## quantile regression
-if (run == TRUE){
-    ctrl <- lqmmControl(method = "df", LP_tol_ll = 1e-4, LP_tol_theta = 1e-4, check_theta = TRUE, LP_max_iter = 1000)
-    m14_covs <- c("age*time", "gender", "caste", "rural*time", "educ_dad", "childhood_finance", "childhood_health")
-    m14_form <- paste0("gcp ~ educ + educ*time + ", paste0(model_covs, collapse = " + "))
-    m14 <- lqmm(fixed = as.formula(m1_form), random = ~ 1, group = prim_key, tau = c(0.1,0.25,0.5,0.75, 0.9), data = dt)
-    m14_params <- parameters::parameters(m14)
-    write_rds(m14, paste0(derived_dir, "lqmm_", date, ".rds"))
-    write_rds(m14_params, paste0(derived_dir, "lqmm_params_", date, ".rds"))
-} else {
-    files <- list.files(derived_dir)[grepl("lqmm_params_", list.files(derived_dir))]
-    dates <- ymd(str_extract(files, "[0-9]{4}_[0-9]{2}_[0-9]{2}"))
-    m14 <- read_rds(paste0(derived_dir, "lqmm_", gsub("-", "_", max(dates)), ".rds"))
-    m14_params <- read_rds(paste0(derived_dir, "lqmm_params_", gsub("-", "_", max(dates)), ".rds"))
-}
+m19 <- rq(as.formula(m1_form), data = dt, tau = c(0.1,0.25,0.5,0.75, 0.9))
+m19_params <- summary(m19, se = "boot", cluster = dt[, prim_key]) ## https://ideas.repec.org/a/taf/jnlasa/v112y2017i517p446-456.html
 
 ## individual test items
 get_model_item <- function(outcome_item){
     return(lmer(as.formula(gsub("gcp", outcome_item, m0_form)), data = dt))
 }
 
-m15 <- get_model_item("memory"); m16 <- get_model_item("executive"); m17 <- get_model_item("language"); m18 <- get_model_item("visuospatial")
-m19 <- get_model_item("wr_delayed"); m20 <- get_model_item("lm_delayed"); m21 <- get_model_item("animals"); m22 <- get_model_item("con_praxis"); m23 <- get_model_item("ravens")
+m20 <- get_model_item("memory"); m21 <- get_model_item("executive"); m22 <- get_model_item("language"); m23 <- get_model_item("visuospatial")
+m24 <- get_model_item("wr_delayed"); m25 <- get_model_item("lm_delayed"); m26 <- get_model_item("animals"); m27 <- get_model_item("con_praxis"); m28 <- get_model_item("ravens")
 
 # FORMAT MODEL RESULTS -----------------------------------------------------------
 
@@ -126,14 +118,23 @@ get_coefficients <- function(model_num){
                                 estimate = params[param_select, "mean"], 
                                 lower = params[param_select, "0.025quant"], 
                                 upper = params[param_select, "0.975quant"])
-    } else if (model_num == 14){ ## alternative for quantile regression (m14)
-        params <- m14_params
-        param_select <- grepl("^educ.*\\:time$", params$Parameter)
-        result_dt <- data.table(model_num = paste0(model_num, " - ", params$Component[param_select]), 
-                                parameter = params$Parameter[param_select], 
-                                estimate = params$Coefficient[param_select], 
-                                lower = params$CI_low[param_select], 
-                                upper = params$CI_high[param_select])
+    } else if (model_num == 19){ ## alternative for quantile regression (m19)
+        params <- m19_params
+
+        get_coef <- function(tau_num){
+            selected <- params[[tau_num]]
+            param_select <- grepl("^educ.*\\:time$", row.names(selected$coefficients))
+            select_dt <- data.table(model_num = paste0(model_num, " - ", selected$tau), 
+                                    parameter = row.names(selected$coefficients)[param_select], 
+                                    estimate = data.frame(selected$coefficients)$Value[param_select], 
+                                    lower = data.frame(selected$coefficients)$Value[param_select]-1.96*data.frame(selected$coefficients)$`Std..Error`[param_select], ## assume CLT, CI symmetry
+                                    upper = data.frame(selected$coefficients)$Value[param_select]+1.96*data.frame(selected$coefficients)$`Std..Error`[param_select])
+            return(select_dt)
+        }
+
+        result_dt <- rbindlist(lapply(1:length(params), get_coef))
+
+
     } else { ## all other models
         params <- parameters::parameters(model)
         param_select <- grepl("^educ.*\\:time$", params$Parameter)
@@ -147,7 +148,7 @@ get_coefficients <- function(model_num){
     return(result_dt)
 }
 
-model_results <- rbindlist(lapply(0:23, get_coefficients))
+model_results <- rbindlist(lapply(0:28, get_coefficients))
 
 # FORMAT GRAPH DATA -----------------------------------------------------------
 
@@ -161,13 +162,14 @@ model_results[, educ_cat := gsub("educ", "", parameter)][, educ_cat := gsub(":ti
 label_dt <- data.table(model_num = model_results[, unique(model_num)], 
                        labels = c("Mixed effects model (base)", "Weighted GEE model (survey + death)", "Weighted GEE model (survey + attrition)", 
                                   "Weighted GEE model (survey + death + attrition)", "Joint model", "Age (60-69)", "Age (70-79)", 
-                                  "Age (80+)", "Urbanicity (rural)", "Urbanicity (urban)", 
+                                  "Age (80+)", "Urbanicity (rural)", "Urbanicity (urban)", "Gender (men)", "Gender (women)",
                                   "W1 cognition (below median W1 cog)", "W1 cognition (above median W1 cog)", 
                                   "W2 follow-up (below median follow-up)", "W2 follow-up (above median follow-up)", 
+                                  "W1 CDR (normal)", "W1 CDR (MCI/Questionable)", "W1 CDR (dementia)",
                                   "10th percentile", "25th percentile", "50th percentile", "75th percentile", "90th percentile",
                                   "Memory", "Executive functioning", "Language", "Visuospatial functioning", 
                                   "Delayed word recall", "Delayed story recall (logical memory)", "Animal naming", "Constructional praxis", "Raven's progressive matrices"), 
-                       category = c(rep("Base models",5), rep("Stratified", 9), rep("Quantiles", 5), rep("Other cognitive outcomes", 9)))
+                       category = c(rep("Base models",5), rep("Stratified", 14), rep("Quantiles", 5), rep("Other cognitive outcomes", 9)))
 label_dt[, labels := factor(labels, levels = rev(label_dt[, labels]))]                       
 model_results <- merge(model_results, label_dt, by = "model_num", sort = FALSE)
 
@@ -188,7 +190,7 @@ get_graphs <- function(c){
     if (c == "Other cognitive outcomes"){
         plot <- plot + labs(y = "Difference in rate of annual cognitive decline (Ref: No schooling)") + theme(legend.position = "bottom")
     }
-    if (!c == "Base"){
+    if (!c == "Base models"){
         plot <- plot + theme(strip.text = element_blank())
     }
     if (!c == "Other cognitive outcomes"){
@@ -210,9 +212,6 @@ labels <- lapply(model_results[, unique(category)], get_labels)
 
 ## combine plots
 full_plot <- plots[[1]] + labels[[1]] + plots[[2]] + labels[[2]] + plots[[3]] + labels[[3]] + plots[[4]] + labels[[4]] +
-    plot_layout(nrow = 4, widths = c(1, 0.1), heights = c(1,1.5,0.9,1.5))
+    plot_layout(nrow = 4, widths = c(1, 0.1), heights = c(1,2.3,0.9,1.5))
 
-
-## try quantreg
-x <- rq(as.formula(m1_form), data = dt, tau = c(0.1,0.25,0.5,0.75, 0.9))
-y <- summary(x, se = "boot", cluster = dt[, prim_key]) ## https://ideas.repec.org/a/taf/jnlasa/v112y2017i517p446-456.html
+ggsave(paste0(plot_dir, "regression_comparison_", date, ".pdf"), plot= full_plot, height = 8, width = 15)
