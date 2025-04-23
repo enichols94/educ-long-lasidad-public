1ba1c6b1f42b49556126cddcda57165ddaf911d4
Emma Nichols <emmanich@dts.usc.edu>
2025-04-23 16:32:41 -0700

    remove missing data, show unweighted statistics, add sample size

--- 020_descriptivetable.R
+++ 020_descriptivetable.R
@@ -23,7 +23,7 @@ longitudinal_dir <- paste0(dropbox_dir, "H_DAD/Raw_wave2/Preliminary LASI-DAD-Co
 exit_dir <- paste0(dropbox_dir, "H_DAD/Raw_wave2/Combined/Data/Clean/")
 rawdata_dir <- paste0(dir, "data/source/")
 derived_dir <- paste0(dir, "data/derived/")
-plot_dir <- paste0(dir, "plots/")
+table_dir <- paste0(dir, "paper/table1/")
 
 # READ DATA ------------------------------------------------------------------
 
@@ -33,6 +33,13 @@ longitudinal_dt <- data$longitudinal
 
 dt <- copy(survival_dt)
 dt <- merge(dt, longitudinal_dt[wave == 1, .(prim_key, gcp, memory, executive, language, visuospatial)], by = "prim_key")
+
+## remove missing data
+model_covs <- c("age", "gender", "caste", "rural", "educ_dad", "childhood_finance", "childhood_health")
+for (var in c("gcp", "educ", model_covs)){
+    message(paste0("Missing individuals in ", var, ": ", dt[is.na(get(var)), length(unique(prim_key))]))
+    dt <- dt[!is.na(get(var))]
+}
 
 design <- dt[!died == 1 & !refused == 1 & !attrited == 1] %>% as_survey_design(ids = 1, weights = full_weight)
 
@@ -46,7 +53,7 @@ dt[, outcome := factor(case_when(inwave2 == 1 ~ "In wave 2",
 
 # CREATE TABLE ----------------------------------------------------------------
 
-## weighted/unweighted with combo weight (sampling, death, attrition)
+## using unweighted statistics
 
 ## outcome, follow-up time, age, gender, caste, rural, educ_mom, educ_dad, childhood_finance_childhood_health
 ## improvedsanitation, puccahouse, wealthq, consumptionq
@@ -61,22 +68,6 @@ get_col <- function(educ_cat){
     }
 
     ## HELPER FUNCTIONS
-    get_continuous <- function(char, num = 1){
-        west <- d %>% srvyr::summarize(mean = survey_mean(get(char), na.rm = TRUE))
-        est <- data[, mean(get(char), na.rm = TRUE)]
-        wsd <- d %>% srvyr::summarize(sd = survey_sd(get(char), na.rm = TRUE))
-        sd <- data[, sd(get(char), na.rm = TRUE)]
-        paste0(sprintf(paste0("%.", num, "f"), est), " (", sprintf(paste0("%.", num, "f"), sd), ") / ",
-               sprintf(paste0("%.", num, "f"), west[1]), " (", sprintf(paste0("%.", num, "f"), wsd[1]), ")")
-    }
-
-    get_binary <- function(char, cat){
-        west <- d %>% srvyr::summarize(mean = survey_mean(get(char)==cat, proportion = T, na.rm = T))*100
-        est <- nrow(data[get(char) == cat])/nrow(data)*100
-        N <- nrow(data[get(char) == cat])
-        paste0(sprintf("%.1f", est), " / ", sprintf("%.1f", west[1]), " (", nrow(data[get(char)==cat]), ")")
-    } 
-
     get_binary_raw <- function(char, cat){
         paste0(sprintf("%.1f", nrow(data[get(char) == cat])/nrow(data)*100), " (", nrow(data[get(char) == cat]), ")")
     }
@@ -87,20 +78,21 @@ get_col <- function(educ_cat){
     }
 
     ## GET VALUES
+    N <- nrow(data)
     outcome <- sapply(dt[, levels(outcome)], function(x) get_binary_raw("outcome", x))
     followup <- get_continuous_raw("time_elapsed", df = data[inwave2 == 1])
     
-    age <- get_continuous("age")
-    female <- get_binary("female", 1)
-    rural <- get_binary("rural", 1)
-    caste <- sapply(dt[, levels(caste)], function(x) get_binary("caste", x))
+    age <- get_continuous_raw("age")
+    female <- get_binary_raw("female", 1)
+    rural <- get_binary_raw("rural", 1)
+    caste <- sapply(dt[, levels(caste)], function(x) get_binary_raw("caste", x))
 
-    educ_mom <- sapply(dt[, levels(educ_mom)], function(x) get_binary("educ_mom", x))
-    educ_dad <- sapply(dt[, levels(educ_dad)], function(x) get_binary("educ_dad", x))
-    child_finance <- sapply(dt[, levels(childhood_finance)], function(x) get_binary("childhood_finance", x))
-    child_health <- sapply(dt[, levels(childhood_health)], function(x) get_binary("childhood_health", x))
+    educ_mom <- sapply(dt[, levels(educ_mom)], function(x) get_binary_raw("educ_mom", x))
+    educ_dad <- sapply(dt[, levels(educ_dad)], function(x) get_binary_raw("educ_dad", x))
+    child_finance <- sapply(dt[, levels(childhood_finance)], function(x) get_binary_raw("childhood_finance", x))
+    child_health <- sapply(dt[, levels(childhood_health)], function(x) get_binary_raw("childhood_health", x))
 
-    col <- data.table(name = c("", outcome, followup, age, female, rural, "", caste, "", educ_mom, "", educ_dad,
+    col <- data.table(name = c(N, "", outcome, followup, age, female, rural, "", caste, "", educ_dad,
                                "", child_finance, "", child_health))
     setnames(col, "name", educ_cat)
     return(col)
@@ -109,10 +101,10 @@ get_col <- function(educ_cat){
 all_cols <- lapply(c("Overall", dt[, levels(educ)]), get_col)
 table_dt <- Reduce(function(x,y) cbind(x,y), all_cols)
 
-labels <- c("Wave 2 status", paste0("  ", dt[, levels(outcome)]), "Follow-up time", "Age (yrs)", "Female (%)", "Rural (%)", "Caste", 
-            paste0("  ", dt[, levels(caste)]), "Maternal education", paste0("  ", dt[, levels(educ_mom)]), "Paternal education", 
-            paste0("  ", dt[, levels(educ_dad)]), "Childhood financial status", paste0("  ", dt[, levels(childhood_finance)]), 
+labels <- c("N", "Wave 2 status", paste0("  ", dt[, levels(outcome)]), "Follow-up time", "Age (yrs)", "Female (%)", "Rural (%)", "Caste", 
+            paste0("  ", dt[, levels(caste)]), "Paternal education", paste0("  ", dt[, levels(educ_dad)]), 
+            "Childhood financial status", paste0("  ", dt[, levels(childhood_finance)]), 
             "Childhood health status", paste0("  ", dt[, levels(childhood_health)]))
 
 table_dt <- cbind(labels, table_dt)
-write.xlsx(table_dt, paste0(plot_dir, "table1_", date, ".xlsx"))
+write.xlsx(table_dt, paste0(table_dir, "table1_", date, ".xlsx"))
